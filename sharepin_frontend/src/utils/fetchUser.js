fe2bd86c7e5fa51a83b35623fe474d7979861166
Priyanka Gupta <47624791+priyanka1121@users.noreply.github.com>
2023-03-01 20:38:07 +0530

    Add files via upload

---
+++ sharepin_frontend/src/utils/fetchUser.js
@@ -0,0 +1,5 @@
+export const fetchUser = () => {
+  return localStorage.getItem("user") !== "undefined"
+    ? JSON.parse(localStorage.getItem("user"))
+    : localStorage.clear(); // clear when something went wrong like user token expired
+};
